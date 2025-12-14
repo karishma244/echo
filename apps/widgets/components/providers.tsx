3492ad6bb523044a275315aebf2a2258af6fcb20
Karishma <karishmajethwani4@gmail.com>
2025-12-14 23:02:34 +0530

    10:widget screen router

--- apps/widgets/components/providers.tsx
+++ apps/widgets/components/providers.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import {Provider} from "jotai";
 import { ConvexProvider,ConvexReactClient } from "convex/react";
 
 const convex=new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
