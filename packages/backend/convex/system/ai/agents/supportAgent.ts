b4cfd1eeff62a1e2a9c42ac1c79d37c734df477f
Karishma <karishmajethwani4@gmail.com>
2025-12-24 04:19:54 +0530

    18: Ai tool calling

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -2,8 +2,12 @@ import { groq } from "@ai-sdk/groq"; // Import the Groq provider
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api";
 
+
 export const supportAgent = new Agent(components.agent, {
   // Use 'llama-3.3-70b-versatile' on Groq—it is free and very powerful.
   chat: groq("llama-3.3-70b-versatile"), 
-  instructions: "You are a customer support agent",
+  instructions: `You are a customer support agent. Use "resolveConversation" tool when user
+  expresses finalization of the conversation. Use "escalateConversation" tool when user expresses
+  frustration,or requests a human explicitly.`,
+  
 });
