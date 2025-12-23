6ef5454010c6947c92399a15d201e69701e59ae8
Karishma <karishmajethwani4@gmail.com>
2025-12-23 23:40:13 +0530

    17:Dashboard chat

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -3,23 +3,27 @@ import { OrganizationGuard } from "@/modules/auth/ui/components/organization-gua
 import { SidebarProvider} from "@workspace/ui/components/sidebar";
 import { cookies } from "next/headers";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import { Provider  } from "jotai";
 export const DashboardLayout= async({children}:{children:React.ReactNode}) => {
     const cookieStore= await cookies();
     //to collapse the sidebar or not and when we collapse backend should know
     const defaultOpen=cookieStore.get("sidebar_state")?.value==="true";
     return(
+
         <AuthGuard>
             <OrganizationGuard>
+                <Provider>
                 <SidebarProvider defaultOpen={defaultOpen}>
                       <div className="flex h-screen w-screen overflow-hidden">
                 <DashboardSidebar/>
                 <main className="flex-1 h-full overflow-hidden">
                
-        {children}
+                {children}
       
                 </main>
               </div>
                 </SidebarProvider>
+                </Provider>
             </OrganizationGuard>
         </AuthGuard>
     );
