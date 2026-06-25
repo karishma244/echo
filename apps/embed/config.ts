e8ed0f6cab0113d31d0e41f944b7302d4a69e6a2
karishma244 <karishmajethwani4@gmail.com>
2026-06-26 03:39:04 +0530

    34: embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+  DEFAULT_ORG_ID: "org_36hRM5kKxju3ROfciZ8ri0H5pwn",
+  DEFAULT_POSITION: "bottom-right" as const,
+};
